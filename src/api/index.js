9350a525e7b5677f8a5c1be7ea201e99b8160cd6
ellapresso <ellapresso@gmail.com>
2019-11-24 19:53:44 +0900

    파일 테스트

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,17 @@ const router = require('express').Router();
 const fs = require('fs');
 
 router.post('/result',(req,res) => {
-    fs.writeFile('text.txt', 'data2', 'utf8', function(err) {
-        console.log('비동기적 파일 쓰기 완료');
+
+    fs.readFile('text.txt', (err,data)=>{
+        console.log('data : ' + data)
+        //데이터를 읽어온 후
+        fs.writeFile('text.txt', 'data', 'utf8', function(err) {
+            //파일을 저장한다.
+            console.log('비동기적 파일 쓰기 완료');
+        });
     });
+
+
 
     return res.send(200);
 });
