003a0b66113dcac02c693f779e4948892463e6da
ellapresso <ellapresso@gmail.com>
2019-11-30 12:42:00 +0900

    TODO

--- src/api/index.js
+++ src/api/index.js
@@ -7,15 +7,16 @@ const name = req.query.name + '#' + `${new Date().getTime()}`;
     
     fs.readFile('rank.json','utf8', (err, data)=>{
         data = JSON.parse(data);
+        // 불러온 data와, 입력받은 point 를 sorting한 다음, 새 배열에 상위 10개만 저장후 writeFile에 넘긴다.
+        //TODO: 불러온 데이터와 입력받은 데이터를 비교후, 상위 10개만 남긴다.
         data.push({name,point})
-        //데이터를 읽어온 후
+
         fs.writeFile('rank.json', JSON.stringify(data), 'utf8', function(err) {
-            //파일을 저장한다.
             console.log('비동기적 파일 쓰기 완료');
+            return res.send(data);
         });
     });
 
-    return res.send(200);
 });
 
 
